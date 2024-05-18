49840ca38cd8e1992432b3d8460f38ce138e2224
Justineaddo <jaddo036@st.ug.edu.gh>
2024-05-18 07:08:35 -0700

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -11,3 +11,19 @@ function processArray(arr) {
   }
   let numbers = [1, 2, 3, 4, 5, 6];
 console.log(processArray(numbers));
+
+
+function formatArrayStrings(strings, numbers) {
+  for (let i = 0; i < strings.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      strings[i] = strings[i].toUpperCase();
+    } else {
+      strings[i] = strings[i].toLowerCase();
+    }
+  }
+  return strings;
+}
+
+let strings = ["Stanley", "fish", "Wool", "table","push"];
+let numberss = [1, 2, 3];
+console.log(formatArrayStrings(strings, numbers));
